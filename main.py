9e3e907a3d316f2b60bcee8eb1df5f7337bde1d5
Miki Lovrenovic <lovrenovic.miki@gmail.com>
2024-01-31 02:25:37 +0100

    Load the excel data

---
+++ main.py
@@ -0,0 +1,8 @@
+from fpdf import FPDF
+import pandas as pd
+import glob
+
+filepaths = glob.glob("Invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
